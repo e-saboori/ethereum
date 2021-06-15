2bb4ee3d81d0b76dc2aad9e89caf87ccbefccc37
e-saboori <33030620+e-saboori@users.noreply.github.com>
2021-06-15 09:59:12 -0400

    Update Administrator.sol

--- Administrator.sol
+++ Administrator.sol
@@ -1,16 +1,19 @@
 // Administrator.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import "./SmartContract.sol";
 
 contract Administrator{
 
+    SmartContract _contract;
+    
     struct Account{
         uint256 balance;
         bool isRegistered;
     }
     
-    //mapping(uint256=>address) _tokensOwner;
-    mapping(address=>Account) _traders;
+    // this can be separated to seller and buyer...
+    mapping(address=>Account) _traders; 
     
     function Register(address trader, uint256 balance) public {
         if(trader!=address(0) && _traders[trader].isRegistered == false){
@@ -37,15 +40,15 @@ contract Administrator{
         return false;
     }
     
-    function TransferFrom(address sender, address recipient, uint256 amount) public{
+
+    function TransferFrom(address sender, address recipient,uint256 tokenId, uint256 amount) public{
         if(sender!=address(0) && recipient!= address(0))
         {
             require(IsTraderRegistered(sender), "Buyer is not registered");
             require(IsTraderRegistered(recipient), "Seller is not registered");
             require(HasTraderEnoughBalance(sender, amount), "Buyer does not have sufficient balance");
-            
-            _traders[sender].balance -= amount;
-            _traders[recipient].balance += amount;
+
+            _contract.transferFrom(sender, recipient, tokenId, amount);
         }
     }
 }
