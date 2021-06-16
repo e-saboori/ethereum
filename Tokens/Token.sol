18169fc1c6b5357c69e7ccfe1d0700b11f139f6d
e-saboori <33030620+e-saboori@users.noreply.github.com>
2021-06-16 10:59:58 -0400

    Update Token.sol

--- Tokens/Token.sol
+++ Tokens/Token.sol
@@ -6,12 +6,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
     string __name;
-    constructor(uint256 initialSupply) ERC20("token", "TNK") {
+    constructor(string memory _name, string memory _symbol, uint256 initialSupply) ERC20(_name, _symbol) {
         _mint(msg.sender, initialSupply);
-    }
-    
-    function SetName(string memory name) public{
-        __name = name;
     }
     
     function decimals() public view virtual override returns (uint8) {
