1656d800990eed74d6fcb398f1aba14eaf7aecc8
e-saboori <33030620+e-saboori@users.noreply.github.com>
2021-06-16 10:38:39 -0400

    Update Administrator.sol

--- Administrator.sol
+++ Administrator.sol
@@ -1,29 +1,32 @@
 // Administrator.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "./SmartContract.sol";
+import "./Tokens/Token.sol";
 
 contract Administrator{
-
-    SmartContract _contract;
+    
+    address __admin;
+    mapping(address => Account) __traders; 
     
     struct Account{
         uint256 balance;
+        mapping(uint256 => Token) tokens;
         bool isRegistered;
     }
-    
-    // this can be separated to seller and buyer...
-    mapping(address=>Account) _traders; 
+ 
+    constructor(){
+        __admin = msg.sender;
+    }
     
     function Register(address trader, uint256 balance) public {
-        if(trader!=address(0) && _traders[trader].isRegistered == false){
-            _traders[trader].isRegistered = true;
-            _traders[trader].balance = balance;
+        if(trader!=address(0) && __traders[trader].isRegistered == false){
+            __traders[trader].isRegistered = true;
+            __traders[trader].balance = balance;
         }
     }
     
     function IsTraderRegistered(address trader) public view returns (bool) {
-        if(trader != address(0) && _traders[trader].isRegistered == true){
+        if(trader != address(0) && __traders[trader].isRegistered == true){
             return true;
         }
         return false;
@@ -31,7 +34,7 @@ contract Administrator{
     
     function HasTraderEnoughBalance(address trader, uint256 amount) public view returns (bool){
         if(trader != address(0)){
-            uint256 balance = _traders[trader].balance;
+            uint256 balance = __traders[trader].balance;
             if(balance >= amount){
                 return true;
             }
@@ -39,16 +42,26 @@ contract Administrator{
         
         return false;
     }
-    
 
-    function TransferFrom(address sender, address recipient,uint256 tokenId, uint256 amount) public{
-        if(sender!=address(0) && recipient!= address(0))
-        {
+    function TransferFrom(
+        address sender, 
+        address payable recipient,
+        uint256 tokenId,
+        uint256 tokenPrice,
+        uint256 amount) 
+    public{
             require(IsTraderRegistered(sender), "Buyer is not registered");
             require(IsTraderRegistered(recipient), "Seller is not registered");
             require(HasTraderEnoughBalance(sender, amount), "Buyer does not have sufficient balance");
-
-            _contract.transferFrom(sender, recipient, tokenId, amount);
-        }
+            
+            Token token = __traders[recipient].tokens[tokenId];
+            recipient.transfer(amount);
+            token.transfer(sender, amount* 10^token.decimals());
+            
+            __traders[sender].tokens[tokenId] = token;
+            delete __traders[recipient].tokens[tokenId];
+            __traders[sender].balance -= amount* tokenPrice;
+            __traders[recipient].balance += amount * tokenPrice;
+            
     }
 }
